8a98e24059c7bd547b9fad672b0f9e1699caa89e
Katrina Lara <111546742+ktrnlara@users.noreply.github.com>
2023-06-15 08:23:13 +0800

    Create Main.java

---
+++ Updated Bulldogs Exchange System Management/packageBulldogsExchange/Main.java
@@ -0,0 +1,10 @@
+package BulldogsExchange;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HomeFrame home = new HomeFrame("NU Bulldog Exchange Queueing Management System");
+//        ServiceFrame home = new ServiceFrame("NU Bulldog Exchange Queueing Management System", "");
+
+    }
+}
